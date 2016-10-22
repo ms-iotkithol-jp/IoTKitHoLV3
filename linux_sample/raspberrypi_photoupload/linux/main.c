4b85fc42fb9a4e25261e7ba95c033905454fdfdb
embeddedgeorge <embedded-george>
2016-10-22 12:58:27 +0900

    Raspbian Sample 1st release
    IoTHub Messaging
    Photo Upload

---
+++ linux_sample/raspberrypi_photoupload/linux/main.c
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "raspberrypi_photoupload.h"
+
+int main(void)
+{
+    raspberrypi_photoupload_run();
+}
---
+++ linux_sample/raspberrypi_photoupload/raspberrypi_photoupload.h
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef RASPBERRYPI_PHOTOUPLOAD_H
+#define RASPBERRYPI_PHOTOUPLOAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void raspberrypi_photoupload_run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RASPBERRYPI_PHOTOUPLOAD_H */
