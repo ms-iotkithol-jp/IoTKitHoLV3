5628afd4c86f669d0f3ee952823273a2b9fd5ead
Atomu Hidaka <ahidaka@devdrv.co.jp>
2017-03-09 12:48:22 +0900

    CC2650STK Branch created

--- WinIoTCoreTIIoTHubApp/TISensorTagLibrary/SensorName.cs
+++ WinIoTCoreTIIoTHubApp/TISensorTagLibrary/SensorName.cs
@@ -18,10 +18,12 @@ namespace TISensorTagLibrary
         Accelerometer,
         Gyroscope,
         HumiditySensor,
+        LightSensor,
         TemperatureSensor,
         Magnetometer,
         PressureSensor,
-        SimpleKeyService
+        SimpleKeyService,
+        IOSerivice
     }
 
     public enum TemperatureScale
@@ -30,4 +32,12 @@ namespace TISensorTagLibrary
         Farenheit
     }
 
+    public enum AccelRange
+    {
+        //http://processors.wiki.ti.com/index.php/CC2650_SensorTag_User's_Guide#Movement_Sensor
+        _2G = 0,
+        _4G = 1,
+        _8G = 2,
+        _16G = 3
+    }
 }
